aeccd81111581f70cfc2d139e6aa69a29844b9b8
jmkerloch <53606373+jmkerloch@users.noreply.github.com>
2021-01-21 13:53:02 +0100

    define wxWidget config prefix option

--- cmake/additionnal-definitions.cmake
+++ cmake/additionnal-definitions.cmake
@@ -33,6 +33,11 @@ if(BUILD_wxWidgets OR USE_PRECOMPILED_EXT)
   # Define wxWidgets_ROOT_DIR
   set(wxWidgets_ROOT_DIR ${DEPS_INSTALL_DIR})
   
-  set(wxWidgets_USE_STATIC ON)
+  # Define specif wxWidget config option so wxWidget is found with find_package
+  if (UNIX)
+      set(wxWidgets_USE_STATIC ON)
+      set(wxWidgets_CONFIG_EXECUTABLE ${DEPS_INSTALL_DIR}/bin/wx-config)
+      set(wxWidgets_CONFIG_OPTIONS --prefix=${DEPS_INSTALL_DIR})
+  endif()
 
 endif()
